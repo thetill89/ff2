c3c7f6d11a3f4e54b5d1917da626c50c138b4f4a
thetill89 <65866654+thetill89@users.noreply.github.com>
2020-09-23 07:51:23 +0200

    Update swarm.js

--- swarm.js
+++ swarm.js
@@ -1,4 +1,4 @@
-var mySpeed = 4, autoBuy = false
+var mySpeed = 1, autoBuy = false
 
 angular.element(document.querySelector('.ng-scope')).scope().game.gameSpeed = mySpeed
 var auto = setInterval(function() { buyUpgrades() }, 1000)
@@ -9,23 +9,25 @@ function buyUpgrades() {
 	document.body.dispatchEvent(key)
 }
 
-createButton(1, 'Speed: ' + mySpeed, 620)
-createButton(2, 'Reset', 520)
-createButton(3, 'Auto: off', 420)
+createButton(1, 'Reset', 620)
+createButton(2, 'Speed: ' + mySpeed, 520)
+createButton(3, 'Autobuy', 420)
 b1.onclick = function() {
+	mySpeed = 1
+	b1.innerText = 'Speed: ' + mySpeed
+	angular.element(document.querySelector('.ng-scope')).scope().game.gameSpeed = mySpeed
+}
+b2.onclick = function() {
 	mySpeed *= 4
 	if (mySpeed > 64) mySpeed = 1
 	this.innerText = 'Speed: ' + mySpeed
 	angular.element(document.querySelector('.ng-scope')).scope().game.gameSpeed = mySpeed
 }
-b2.onclick = function() {
-	mySpeed = 1
-	b1.innerText = 'Speed: ' + mySpeed
-	angular.element(document.querySelector('.ng-scope')).scope().game.gameSpeed = mySpeed
-}
+
 b3.onclick = function() {
 	autoBuy = !autoBuy
-	b3.innerText = 'Auto: ' + (autoBuy ? 'on' : 'off')
+	if (autoBuy) { this.style.backgroundColor = '#2a9fd6'
+	else this.style.backgroundColor = '#black'
 }
 
 function createButton(id, txt, right = 0 , width = 90) {
@@ -40,9 +42,9 @@ function createButton(id, txt, right = 0 , width = 90) {
 	btn.style.color = 'white'
 	btn.style.position = 'absolute'
 	btn.style.right = right + 'px'
-	btn.style.top = '5px'
+	btn.style.top = '0px'
 	btn.style.width =  width + 'px'
-	btn.style.height =  '32px'
+	btn.style.height =  '40px'
 	btn.style.outline = 'none' 
 	btn.style.cursor = 'default'
 	document.getElementsByClassName('ng-scope')[0].appendChild(btn); 
