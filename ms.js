fcfa9a3d43a04b3033b9bc1c52ff766cb1913924
thetill89 <65866654+thetill89@users.noreply.github.com>
2021-04-06 20:30:32 +0200

    Update ms.js

--- ms.js
+++ ms.js
@@ -365,12 +365,17 @@ function statusInfo() {
 	let s = 11
 	info2.value = ''
 	if (todayRuns.r) { 
+		if (!autoSkill && getPoints('skill') < 200) {
+			let sp2 = Math.ceil((200 - getPoints('skill')) / (todayRuns.sp / todayRuns.r)) * 600
+			sp2 = new Date(new Date().setSeconds(sp2))
+			addStatus2(pr('Next SP: ',s) + sp2.toLocaleTimeString().slice(0,5))
+		}	
 		let epRun = todayRuns.ep / todayRuns.r
 		let rp1 = Math.ceil((500 - getPoints('event')) / epRun) * 600, rp10
 		if (getPoints('research') < 9) { 
 			rp10 = (Math.ceil(500 / epRun) * (9 - getPoints('research')) * 600) + rp1
 			rp10 = new Date(new Date().setSeconds(rp10))
-			addStatus2(pr('Ten RP: ',s) + rp10.toLocaleTimeString().slice(0,5))
+			addStatus2(pr('Ten  RP: ',s) + rp10.toLocaleTimeString().slice(0,5))
 		}
 		if (rp1 === 600) rp1 = sessionTime - getSessionTime()
 		rp1 = new Date(new Date().setSeconds(rp1))
@@ -387,6 +392,7 @@ function statusInfo() {
 	let data = getFarmData()
 	let time = (Date.now()-ST.startTime)/1000
 	let runMS = getValue(getLogData()[1]) * todayRuns.r
+	//parseInt(getFarmData()[0]) + Datecheck für 0.00-1.00
 	addStatus2(pr('Points:',s-1) + data[2] + ' fp, ' + data[3] + ' sp, ' + data[4] + ' ep')
 	addStatus2(pr('MS Farm:',s) + data[1] + ', ' + (getValue(data[1]) / (runMS + getValue(data[1])) * 100).toFixed(2) + '%')
 	runMS = (runMS * todayRuns.r)
