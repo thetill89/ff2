27dee9bb7f3f5fd895939601797e8cea677b75e7
thetill89 <65866654+thetill89@users.noreply.github.com>
2021-04-19 20:47:25 +0200

    Update ms.js

--- ms.js
+++ ms.js
@@ -137,7 +137,7 @@ function startIdleMode() {
 		}
 		else addStatus2('Idle max')
 		if (buyStep > 1) getMissionTime()
-		if (buyStep > buyMax.length) {
+		if (buyStep >= buyMax.length) {
 			for (let i = 1; i <= Math.floor((sessionTime-time) / 120); i++) setTimeout(getMissionTime, i * 120 * 1000)
 		}
 	}
